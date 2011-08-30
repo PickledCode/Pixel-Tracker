8220e255abef3103430358c4d4a01df176b24611
Ryan <rsully.dev+git@me.com>
2011-08-29 22:43:49 -0400

    working on submit

--- pixel.submit.php
+++ pixel.submit.php
@@ -2,7 +2,13 @@
 require_once dirname(__FILE__) . '/_classes/_include.php';
 $_response = array();
 
-// Do magic here
+if (isset($_POST['email'])) {
+	$email = $_POST['email'];
+	
+	if (filter_var($email, FILTER_VALIDATE_EMAIL) === true) {
+		
+	}
+}
 
 echo json_encode($_response);
 exit;
