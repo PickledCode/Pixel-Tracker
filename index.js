85c639a08644966ab98e551b220af0eff6f2d1a3
Ryan <rsully.dev+git@me.com>
2011-08-29 22:20:48 -0400

    Started ajax work

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
 	$('#email_form').submit(function(){
 		var email = $('#email_value').val();
+		
+		$.ajax({
+			'url': 'pixel.submit.php',
+			'async': true,
+			'cache': false,
+			'data': {'email': email},
+			'dataType': 'json',
+			'error': function(jqXHR, textStatus, errorThrown) {
+				alert('There has been an error. Refresh and try again?');
+			},
+			'success': function(data, textStatus, xhr) {
+				console.log('You haz success!');
+				console.log(data);
+			}
+		});
+		
 		return false;
 	});
 });
