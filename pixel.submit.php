e90003c2d26455baf123399b4396a152307e9831
Ryan <rsully.dev+git@me.com>
2011-08-29 23:23:31 -0400

    Fixed?

--- pixel.submit.php
+++ pixel.submit.php
@@ -3,11 +3,15 @@ require_once dirname(__FILE__) . '/_classes/_include.php';
 $_response = array();
 
 $_status = 500;
+$_response['step'] = 0;
 
 if (isset($_POST['email'])) {
 	$email = $_POST['email'];
+	$_response['step'] = 1;
 	
 	if (filter_var($email, FILTER_VALIDATE_EMAIL) === true) {
+		$_response['step'] = 2;
+		
 		$encryptor = new Encryption('iHateMonkeys');
 		$encrypt = $encryptor->encrypt($email);
 		$_response['link'] = '/pixel.png?e=' . urlencode($encrypt);
